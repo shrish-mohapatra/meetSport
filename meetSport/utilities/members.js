421d7905934f0591eee0af4aaf59ecc3c83b80dd
yyassin <yyass2@ocdsb.ca>
2020-05-02 12:53:56 -0400

    initialized app skeleton frame with test data.

---
+++ meetSport/utilities/members.js
@@ -0,0 +1,32 @@
+const members = [
+{
+    id: 1,
+    name: 'John Doe',
+    email: 'john@gmail.com',
+    skill: 'beginner',
+    tags: ['soccer', 'tennis', 'basketball']
+},
+{
+    id: 2,
+    name: 'Yousef Yassin',
+    email: 'yousef.yassin@gmail.com',
+    skill: 'advanced',
+    tags: ['soccer', 'tennis', 'basketball']
+},
+{
+    id: 3,
+    name: 'Shrish Mo',
+    email: 'shrishmohapatra@gmail.com',
+    skill: 'intermediate',
+    tags: ['soccer', 'golf', 'basketball', "frisbee"]
+},
+{
+    id: 4,
+    name: 'Michael Kagnew',
+    email: 'michaelkagnew@gmail.com',
+    skill: 'advanced',
+    tags: ['golf', 'tennis', 'basketball']
+}, 
+]
+
+module.exports = members;
